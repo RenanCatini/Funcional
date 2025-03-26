83ef82a3292b265e309015504a3014ae9d897f02
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2025-03-26 19:47:29 -0300

    Aula 3 Funcional

--- Haskell/aula0.hs
+++ Haskell/aula0.hs
@@ -1,29 +1,54 @@
---Nomes de constantes
-type Vendas = Int
+import Data.Char
+
 type Dia = Int
+type Venda = Int
 
---Elevar um número ao quadrado
-quadrado :: Int -> Int
-quadrado x = x * x
+periodo::Int
+periodo = 7
 
---Lista de valores de vendas
-f :: Dia -> Vendas
-f 1 = 2
-f 2 = 15
-f 3 = 7
-f 4 = 11
-f 5 = 3
-f 6 = 4
-f 7 = 1
-f x = 0
 
---Encontrar dia de mais vendas
-maiorV :: Int -> Int -> Int
-maiorV 0 v = v
-maiorV d v
-    |f d > v   = maiorV (d-1) (f d)
-    |otherwise = maiorV (d-1) v
+allEqual :: Int -> Int -> Int -> Bool
+allEqual m n p = (m==n) && (n==p)
 
---Simplificar a chamada de maiorVenda
-maiorVenda :: Int 
-maiorVenda = maiorV 7 0
+maxi :: Int -> Int -> Int
+maxi m n
+   |m >= n = m
+   |otherwise = n
+
+
+f :: Dia -> Venda
+f 1 = 9
+f 2 = 7
+f 3 = 15
+f 4 = 14
+f 5 = 8
+f 6 = 1
+f 7 = 3
+f x = -1
+
+{- função que retorne a maior venda da semana -}
+maiorVenda::Int->Int->Int
+maiorVenda 0 v = v
+maiorVenda d v
+  |f d > v = maiorVenda (d-1) (f d)
+  |otherwise = maiorVenda (d-1) (v)
+
+maiorV7 :: Int
+maiorV7 = maiorVenda periodo 0
+
+{- Exercícios:
+   implemente uma função que retorne o dia em que houve a maior venda (função f)-}
+diaMV :: Int -> Int -> Int
+diaMV p mV 
+    |f p == mV = p
+    |otherwise = diaMV (p-1) mV
+
+
+{- implemente uma função que retorne a quantidade de vendas do período -}
+totalVendas :: Int -> Int
+totalVendas 0 = 0
+totalVendas v = f v + totalVendas (v-1)
+
+{- implemente uma função que retorne a média de vendas-}
+mediaVendas :: Int -> Int
+mediaVendas tV = tV `div` periodo
