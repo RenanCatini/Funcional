e42ca0fe39eb8816cb7cc6009ce6d9a627f22a63
RenanCatini <renan.amaral@unifal-mg.edu.br>
2025-04-25 15:07:27 -0300

    exercicios aula 6 feitos

--- aula6.hs
+++ aula6.hs
@@ -2,14 +2,37 @@ import Data.Char
 
 {-01 função que retorna lista de duplas com char e posição na ASCII -}
 listaDuplaCharInt:: Int-> [(Char,Int)]
+listaDuplaCharInt 0 = []
+listaDuplaCharInt x = listaDuplaCharInt (x-1) ++ [(chr x, x)]
 
 {-02 função meuChar que pesquisa um char pelo int na lista gerada -}
+meuChar :: Int -> [(Char,Int)] -> Char
+meuChar _ [] = error "Valor nao encontrado na lista"
+meuChar x ((c,i):ys)
+    | x == i = c
+    | otherwise = meuChar x ys
 
 {-03 função meuOrd que pesquisa o int pelo char na lista gerada -}
+meuOrd :: Char -> [(Char,Int)] -> Int
+meuOrd _ [] = error "Char nao encontrado na lista"
+meuOrd x ((a,b):cs)
+    | x == a = b
+    | otherwise = meuOrd x cs
 
 {-04 função que ordena uma lista de inteiros -}
 ordenaLista::[Int]->[Int]
 ordenaLista [] = []
+ordenaLista x = ordenaListaAUX x []
+
+ordenaListaAUX :: [Int] -> [Int] -> [Int]
+ordenaListaAUX [] x = x
+ordenaListaAUX (a:b) c = ordenaListaAUX b (insere a c)
+
+insere :: Int -> [Int] -> [Int]
+insere x [] = [x]
+insere x (a:b)
+    | x > a = a : insere x b
+    | otherwise = x:(a:b)
 
 {-05 seja o tipo [(Bool, [Int])]. 
 Faça uma função que ordena [Int] quando o booleano é True. 
@@ -20,3 +43,6 @@ retorna:                  [(False,[0,1,3,4,9]),(False,[]),(False,[0,1,2,3,4])]
 
 ordenaListaDupla::[(Bool, [Int])]->[(Bool, [Int])]
 ordenaListaDupla [] = []
+ordenaListaDupla ((b,l):xs)
+    | b = (False,ordenaLista l) : ordenaListaDupla xs
+    | otherwise = (False,l) : ordenaListaDupla xs
